e45d960976fe68ed35928dfddc6a0728606f09f7
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-11-23 23:00:07 -0600

    Updates

---
+++ precache-manifest.0e14800f8753351e7ce7ab7e11af3724.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38f67fbd83a80adf58793a61ac5171e",
+    "url": "/RosaIsela31/LabCar-React/index.html"
+  },
+  {
+    "revision": "307c092a3b6ca9b4471d",
+    "url": "/RosaIsela31/LabCar-React/static/css/main.7857663a.chunk.css"
+  },
+  {
+    "revision": "a990144ac5138d131c7c",
+    "url": "/RosaIsela31/LabCar-React/static/js/2.4475de6e.chunk.js"
+  },
+  {
+    "revision": "307c092a3b6ca9b4471d",
+    "url": "/RosaIsela31/LabCar-React/static/js/main.7242d70d.chunk.js"
+  },
+  {
+    "revision": "481ad6af9c9eb562adb2",
+    "url": "/RosaIsela31/LabCar-React/static/js/runtime-main.1c74bfa3.js"
+  },
+  {
+    "revision": "941e4b73acad748482f121b8b6e053cc",
+    "url": "/RosaIsela31/LabCar-React/static/media/taxi.941e4b73.jpg"
+  }
+]);
